39899a6e7b27e029a764937017b50d71ecc2c58d
六步詹皇 <liubuzhanhuang@liubuzhanhuangdeMacBook-Pro.local>
2024-10-30 14:12:55 +0800

    change image resource location

--- src/HomePage/index.js
+++ src/HomePage/index.js
@@ -1,8 +1,7 @@
 import { useEffect, useState } from 'react';
 import Contact from './Contact';
 import Home from './Home';
-import './index.css';
-import bgImage from '../resources/bgImage.jpeg'; 
+import './index.css'; 
 
 function HomePage() {
 
