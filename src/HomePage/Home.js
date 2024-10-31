f6e6c265ef60c585088dd8e238838a2ce50d3e0e
yicong-wang <wangyicong@sandwichlab.ai>
2024-10-31 15:49:52 +0800

    style change

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -1,10 +1,12 @@
 import { useState } from 'react';
+import bgImage from '../assets/images/bgImage.png';
 import './home.css';
 
 function Home() {
 
     return (
         <div className="home">
+            <img src={bgImage} className="image__home"/>
         </div>
     );
 }
