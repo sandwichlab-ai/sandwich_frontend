8573f68e957a4a0b2239796b5a6c88cdf80b289c
Kate605690919 <229026@whut.edu.cn>
2024-11-22 17:51:05 +0800

    project列表页基本流程

--- src/pages/lexi/project/index.jsx
+++ src/pages/lexi/project/index.jsx
@@ -1,13 +1,32 @@
 import { observer } from 'mobx-react-lite';
 import React, { useCallback, useEffect, useMemo } from 'react';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 import { useStore } from '../../../stores/routeStore';
 
 import './index.scss';
-import CardList from '../../../components/card';
+import CardList from '../../../components/lexi-card';
 
 const Project = observer(() => {
   const { projectList } = useStore();
-
+  const operationOptions = useMemo(() => [{
+    key: 'rename',
+    buttonName: 'Rename',
+    icon: <EditOutlined />,
+    modalTitle: 'Do you want to rename this project?',
+    modalContent: `if you delete this project, you won't be able to recover it later`,
+    handleConfirm: (dataItem) => {
+      projectList.removeProject(dataItem.id);
+    }
+  },{
+    key: 'delete',
+    buttonName: 'Delete',
+    icon: <DeleteOutlined />,
+    modalTitle: 'Do you want to delete this project?',
+    modalContent: `if you delete this project, you won't be able to recover it later`,
+    handleConfirm: (dataItem) => {
+      projectList.removeProject(dataItem.id);
+    }
+  }], [])
   const handleAddItem = useCallback(() => {}, []);
   // 初始化projectList
   useEffect(() => {
@@ -16,7 +35,7 @@ const Project = observer(() => {
   // 显示访问list，触发mobx的依赖追踪
   const list = projectList.list.slice(0);
   return (
-    <CardList list={list} handleAddItem={handleAddItem} addUrl='add'></CardList>
+    <CardList list={list} handleAddItem={handleAddItem} addUrl='add' editUrl='edit' operationOptions={operationOptions}></CardList>
   );
 });
 
