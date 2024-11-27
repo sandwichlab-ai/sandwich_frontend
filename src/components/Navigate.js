10b996ddd5523616ec8047f91ffe79a3fd0478fc
yicong-wang <wangyicong@sandwichlab.ai>
2024-11-27 14:29:03 +0800

    add company tag

--- src/components/Navigate.js
+++ src/components/Navigate.js
@@ -7,7 +7,7 @@ function Navigate(props) {
     const [naviState, setNaviState] = useState(0);
 
     useEffect(() => {
-      console.log("666 is small window changed")
+        console.log("666 is small window changed")
     }, [props.isSamllWindow])
 
     const navigate = useNavigate();
@@ -15,36 +15,37 @@ function Navigate(props) {
     const renderText = "Home"
 
     return (
-<div className = "header">
+        <div className="header">
 
-<div className = "header__left" onClick={()=> { 
-    navigate("/"); 
-    setNaviState(0)
-    }}>
-        <img src={image} width="64" height="48"/>
-</div>
-        <div className="header__btn--group">
-                    <span 
-                        onClick={() => props.setIsHome(true)}
-                        style={{ 
-                            color: props.isHome ? '#8C68FF' : '#999999',
-                            fontWeight: props.isHome ? 700 : 500
-                        }}
-                        // className = {props.isHome? 'selected' : 'remain'}
-                        className={`selected ${props.isHome? 'active' : ''}`}
-                        >{renderText}</span>
-                        <span className="gap"></span>
-                    <span 
-                        onClick={() => props.setIsHome(false)}
-                        style={{ 
-                            color: props.isHome ? '#999999' : '#8C68FF',
-                            fontWeight: props.isHome ? 500 : 700 
-                        }}
-                        // className = {props.isHome? 'remain' : 'selected'}
-                        className={`remain ${props.isHome? '' : 'active'}`}
-                        >Contact Us</span>
-                </div>
-                </div>
+            <div className="header__left" onClick={() => {
+                navigate("/");
+                setNaviState(0)
+            }}>
+                <img src={image} width="64" height="48" />
+            </div>
+            <div className="header__btn--group">
+                <span
+                    onClick={() => props.setIsHome(true)}
+                    style={{
+                        color: props.isHome ? '#8C68FF' : '#999999',
+                        fontWeight: props.isHome ? 700 : 500
+                    }}
+                    // className = {props.isHome? 'selected' : 'remain'}
+                    className={`selected ${props.isHome ? 'active' : ''}`}
+                >{renderText}</span>
+                <span className="gap"></span>
+                <span
+                    onClick={() => props.setIsHome(false)}
+                    style={{
+                        color: props.isHome ? '#999999' : '#8C68FF',
+                        fontWeight: props.isHome ? 500 : 700
+                    }}
+                    // className = {props.isHome? 'remain' : 'selected'}
+                    className={`remain ${props.isHome ? '' : 'active'}`}
+                >Contact Us</span>
+            </div>
+            <div className="header__right">Sandwich Lab AI HK Limited</div>
+        </div>
     )
 }
 
